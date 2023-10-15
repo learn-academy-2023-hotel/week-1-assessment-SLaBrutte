5e649a97b0fe6f88a17ecd46163aa9f0db859f4e
SlaBrutte <samlabrutte@yahoo.com>
2023-10-14 17:50:45 -0700

    commit 2

--- code-challenges.js
+++ code-challenges.js
@@ -24,22 +24,48 @@ const fruit2 = "banana"
 
 // --------------------1) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// const aaa = (padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns))
+// console.log(aaa.length)
+
 // Expected output: 9
 
 // Pseudo code:
+//For line 29 im setting a varible that combines the array of lines 27 and 28 together using .concat
+//Line 30. Since im combined my arrays together. I can call upon the set variable and take its total length = 9.
+
+
 
 // --------------------2) Create the code that will reverse the letters of a string using the test variable provided below.
 
-const currentCohort = "Golf 2023"
+// const currentCohort = "Golf 2023"
+// const aaa = currentCohort.split("")
+// const bbb = aaa.reverse()
+// const ccc = bbb.join("")
+
+// console.log(ccc)
 // Expected output: "3202 floG"
 
+
 // Pseudo code:
+//Use the split command to turn my string into an array because you cant reverse and array
+//Line 44 turns my array into a reverse order
+//Line 45 turns my array back into a string 
+//out puts in reverse order
 
 // --------------------3) Create the code that will log only the odd numbers from the array using the test variable provided below.
 
-const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
+// const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
+// for (let i = 0; i < stockExchange.length; i++){
+//     if (stockExchange[i] % 2 !== 0){
+//         console.log(stockExchange[i])
+    // }
+// }
 // Expected output: 13 5 -5 27
 
 // Pseudo code:
+//I use a loop to run my function till the end of its set array
+//Line 60. setting my parameters for my loop to run
+//says my index of my array cannot have a remainder and if it does to print the index out
+
--- coding-concepts.js
+++ coding-concepts.js
@@ -18,16 +18,16 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: When you're taking the length of a string it takes into consideration both the characters and spaces.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: When finding the index within the string it goes to the 4th place holder.
 
 // --------------------3) What will this log?
 
@@ -35,21 +35,24 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Ruby"
+// b) Verify and explain: indexs start at 0 which = "Javascript" so 1 must = "Ruby"
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: type error.
+// b) Verify and explain: You would have to change the array to a string then back to an array
+// so you would end up using .split("") -> .toUpperCase()-> join("")
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+//console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: number
+// b) Verify and explain: It comes out number because it's takes the lenght of the array first = 4. then its asking for the 
+// data type of 4. which comes out to be a number. 
+
